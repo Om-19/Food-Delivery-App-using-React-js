1fb4b4740178013232d003a63b80fe6b46dc92f8
Om-19 <patilom1954@gmail.com>
2024-08-09 11:14:10 +0530

    Home page created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes} from 'react-router-dom'
+import Home from './pages/Home/Home'
 
 function App() {
   return (
     <>
       <div className="app">
         <Navbar />
+        <Routes>
+          < Route path='/' element={<Home />} />
+        </Routes>
       </div>
     </>
   )
